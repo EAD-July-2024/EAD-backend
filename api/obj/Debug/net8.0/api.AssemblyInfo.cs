4899fc837f23b9ebfd5e3b292b297d1fd811ef8f
IT21018596 <it21018596@my.sliit.lk>
2024-09-29 13:33:55 +0530

    development push

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6057429d4ca791cee98867442cdc2cbe76d9b6c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c14e6d16455869db26662f33263adf323c2f3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
