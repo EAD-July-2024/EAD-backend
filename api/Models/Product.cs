ab62c187619fa719d50f1e457f1eb836f04f41da
Udesh Piyumantha <it21013928@my.sliit.lk>
2024-10-02 14:46:26 +0530

    fixed id issue

--- api/Models/Product.cs
+++ api/Models/Product.cs
@@ -1,16 +1,16 @@
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
-namespace api.Models
+public class Product
 {
-    public class Product
-    {
-        public ObjectId Id { get; set; }
-        public string Name { get; set; } = null!;
-        public string Description { get; set; } = null!;
-        public string Price { get; set; } = null!;
-        public string CategoryID { get; set; } = null!;
-        public string VendorID { get; set; } = null!;
-        public List<string> ImageUrls { get; set; } = new List<string>();
-        public bool IsActive { get; set; }
-    }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]  // This ensures MongoDB treats _id as a string in serialization
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Price { get; set; }
+    public string CategoryID { get; set; }
+    public string VendorID { get; set; }
+    public List<string> ImageUrls { get; set; } = new List<string>();
+    public bool IsActive { get; set; }
 }
