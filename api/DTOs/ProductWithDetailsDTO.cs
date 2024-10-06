c05dbb7040a0ff29835cc9e40641bd0c7084d6d3
Kithmina Siriwardana <it21020230@my.sliit.lk>
2024-10-06 14:50:07 +0530

    products get updated

---
+++ api/DTOs/ProductWithDetailsDTO.cs
@@ -0,0 +1,18 @@
+namespace api.DTOs
+{
+    public class ProductWithDetailsDTO
+    {
+        public string Id { get; set; }
+        public string ProductId { get; set; } = null!;
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string VendorId { get; set; }
+        public string VendorName { get; set; }
+        public List<string> ImageUrls { get; set; } = new List<string>();
+
+    }
+}
