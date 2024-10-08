38f23c24bbe61b9b9f2f0b942c93ec007f5e6ab8
Kithmina Siriwardana <it21020230@my.sliit.lk>
2024-10-08 16:28:46 +0530

    order by vendor

--- api/Services/OrderRepository.cs
+++ api/Services/OrderRepository.cs
@@ -37,6 +37,30 @@ namespace api.Services
         {
             return await _orders.Find(order => order.OrderId == orderId).FirstOrDefaultAsync();
         }
+
+        // Get Orders by a list of Order IDs
+        public async Task<List<Order>> GetOrdersByIdsAsync(List<string> orderIds)
+        {
+            return await _orders.Find(o => orderIds.Contains(o.OrderId)).ToListAsync();
+        }
+
+
+        // Get orders by Vendor ID
+        // public async Task<List<Order>> GetOrdersByVendorIdAsync(string vendorId)
+        // {
+        //     // Fetch all OrderItems associated with the given VendorId
+        //     var orderItems = await _orderItemRepository.Find(oi => oi.VendorId == vendorId).ToListAsync();
+
+        //     // Get a list of distinct OrderIds from the fetched OrderItems
+        //     var orderIds = orderItems.Select(oi => oi.OrderId).Distinct().ToList();
+
+        //     // Fetch the orders that match the OrderIds
+        //     var orders = await _orders.Find(o => orderIds.Contains(o.OrderId)).ToListAsync();
+
+        //     return orders;
+        // }
+
+
         // Update an existing order
         public async Task UpdateOrderAsync(Order order)
         {
